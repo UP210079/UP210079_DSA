d3dbb6cd7838956df35bfcbb09ca7f9f8a98d7ba
José Alberto Flores Martínez <113202782+UP210079@users.noreply.github.com>
2023-02-12 19:01:03 -0600

    Delete 06_Palindromo.py

--- U1/06_Palindromo.py
+++
@@ -1,8 +0,0 @@
-word = input("Ingrese una palabra: ")
-word = word.lower()
-word = ''.join(filter(str.isalnum, word))
-
-if word == word[::-1]:
-    print("La palabra es un palíndromo")
-else:
-    print("La palabra no es un palíndromo")
